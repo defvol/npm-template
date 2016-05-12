b92b9171623443fa2786a766a2303346304d8198
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-05-11 22:54:21 -0700

    Add short arguments

--- cli.js
+++ cli.js
@@ -2,9 +2,9 @@ var argv = require('minimist')(process.argv.slice(2));
 var modu = require('./index');
 var util = require('./utils');
 
-if (argv.version) {
+if (argv.version || argv.v) {
   console.log(util.version());
-} else if (argv.help) {
+} else if (argv.help || argv.h) {
   console.log(util.usage());
 } else if (argv.hello) {
   console.log(modu.hello());
